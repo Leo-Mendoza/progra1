5a3b810548a9935cf80db44403c3797d613befbf
User <leonardoemendoza1@gmail.com>
2024-03-17 22:06:57 -0300

    Comienzo de proyecto

---
+++ src/Ej8.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ej8 {
+	static void ponerNota(double x, double y) {
+		double prom = (x + y) / 2;
+		
+		if (prom >= 7) { 
+			System.out.println("Promocionado");
+		}
+		
+		else if (prom >= 4) {
+			System.out.println("Aprobado");
+		}
+		
+		else if (prom < 4) {
+			System.out.println("Debe recuperar");
+		}
+	}
+	
+	public static void main (String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Ingresa la primera nota: ");
+		double num1 = scan.nextDouble();
+		System.out.println("Ingresa la segunda nota: ");
+		double num2 = scan.nextDouble();
+		
+		ponerNota(num1,num2);
+		scan.close();
+	}
+}
